02592bf4f8c5f79d561d6f7a093c845f47e04050
emanpervaiz-cloud <emanemanii306@gmail.com>
2026-03-02 12:05:43 +0500

    Add debug logging to SummarizeSpokenText component

--- src/components/questions/SummarizeSpokenText.jsx
+++ src/components/questions/SummarizeSpokenText.jsx
@@ -5,6 +5,8 @@ import ScoreDisplay from '../common/ScoreDisplay';
 import AIEvaluationService from '../../services/aiEvaluationService';
 
 const SummarizeSpokenText = ({ question, onNext }) => {
+  console.log('SummarizeSpokenText mounted:', { questionId: question?.id, type: question?.type });
+  
   const { saveAnswer } = useExam();
   const [summary, setSummary] = useState('');
   const [wordCount, setWordCount] = useState(0);
